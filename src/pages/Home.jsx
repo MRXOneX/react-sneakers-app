11fa4faff932da7bbd1decbe82ff45455f5c7e3c
MRXOneX <mikhail.poleshchenkov@mail.ru>
2021-07-24 16:31:04 +0300

    Sneakers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import search from '../assets/icons/search.png'
+import remove from '../assets/icons/Remove.svg'
+
+import Card from '../components/Card';
+
+function Home({
+  items,
+  searchValue,
+  setSearchValue,
+  onChangeSearchInput,
+  onAddToFavorite,
+  onAddToCart,
+  isLoading,
+}) {
+  const renderItems = () => {
+    const filtredItems = items.filter((item) =>
+      item.title.toLowerCase().includes(searchValue.toLowerCase()),
+    );
+    return (isLoading ? [...Array(8)] : filtredItems).map((item, index) => (
+      <Card
+        key={index}
+        onFavorite={(obj) => onAddToFavorite(obj)}
+        onPlus={(obj) => onAddToCart(obj)}
+        loading={isLoading}
+        {...item}
+      />
+    ));
+  };
+
+  return (
+    <div className="content p-40">
+      <div className="d-flex align-center justify-between mb-40">
+        <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
+        <div className="search-block">
+          <img src={search} height={16} width={16}  alt="Search" />
+          {searchValue && (
+            <img
+              onClick={() => setSearchValue('')}
+              className="clear cu-p"
+              src={remove}
+              alt="Clear"
+            />
+          )}
+          <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
+        </div>
+      </div>
+      <div className="d-flex flex-wrap">{renderItems()}</div>
+    </div>
+  );
+}
+
+export default Home;
